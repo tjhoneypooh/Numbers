28160f4f9035bf0d1b2be2e3c77357bea160ac66
bshellenbarger <71110804+bshellenbarger@users.noreply.github.com>
2020-12-03 21:26:05 -0500

    pushing brit brit changes

---
+++ index.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+        var num1 = document.getElementById("firstBox").value;
+        var num2 = document.getElementById("secondBox").value;
+        document.getElementById("total").value = num1 * num2;
+}
