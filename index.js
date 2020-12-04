3eda1de18a93e512ebad71ac27988aac87871bf4
Tangie J <tangie.jones16@gmail.com>
2020-12-03 21:26:11 -0500

    tangie

---
+++ index.js
@@ -0,0 +1,7 @@
+function divideBy() 
+{ 
+        let num1 = document.getElementById("box1").value;
+        let num2 = document.getElementById("box2").value;
+        document.getElementById("box3").value = num1/num2;
+
+}
