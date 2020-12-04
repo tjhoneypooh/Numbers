ce11fa17289b49b7deaeecab05b4e02e5ee0512a
Christa Wilson <chrwilso14@yahoo.com>
2020-12-03 21:57:41 -0500

    sub

---
+++ index.js
@@ -0,0 +1,12 @@
+function subAnswer(){
+const numberOne = document.getElementById('firstNumber').value;
+console.log(numberOne);
+const numberTwo = document.getElementById('secondNumber').value;
+console.log(numberTwo);
+document.getElementById('answer').value = numberOne - numberTwo;
+
+
+}
+
+
+
