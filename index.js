c876bdba6438816e3bf86a671e3a82a6ed1d18c3
Tangie J <tangie.jones16@gmail.com>
2020-12-03 21:47:51 -0500

    3minus1

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function divideBy() 
-{ 
-        let num1 = document.getElementById("box1").value;
-        let num2 = document.getElementById("box2").value;
-        document.getElementById("box3").value = num1/num2;
+// addition
+const addTwoNumbers = (num1, num2) => {
+  return num1 + num2;
+};
+const showAdditionAnswer = () => {
+  let firstTxt = document.getElementById("addFirst").value;
+  let secondTxt = document.getElementById("addSecond").value;
+  let firstNum = parseInt(firstTxt);
+  let secondNum = parseInt(secondTxt);
 
+  let addAnswer = addTwoNumbers(firstNum, secondNum);
+
+  document.getElementById("addAnswer").value = addAnswer;
+};
+
+// multiply
+function multiplyBy() {
+  var num1 = document.getElementById("firstBox").value;
+  var num2 = document.getElementById("secondBox").value;
+  document.getElementById("total").value = num1 * num2;
 }
+
+//divide
+function divideBy() {
+    let num1 = document.getElementById("box1").value;
+    let num2 = document.getElementById("box2").value;
+    document.getElementById("box3").value = num1 / num2;
+  }
